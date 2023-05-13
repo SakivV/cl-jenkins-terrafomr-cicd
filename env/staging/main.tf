1fbacae5ce7941c75e44dfb15270497389e6bb8d
vikas <banagevikas@gmail.com>
2023-05-13 14:56:23 +0200

    removed bucket

--- env/staging/main.tf
+++ env/staging/main.tf
@@ -2,8 +2,3 @@ module "s3-static-website" {
   source = "../../module/"
   bucket_name  = "staging-static-website-iamvivek"
 } 
-
-module "s3-static-website-test" {
-  source = "../../module/"
-  bucket_name  = "staging-static-website-iamaws"
-}
