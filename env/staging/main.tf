df0ccc44e73e61ece3103ae3fd4435814c61fda6
vikas <banagevikas@gmail.com>
2023-05-13 14:47:16 +0200

    addded another bucket

--- env/staging/main.tf
+++ env/staging/main.tf
@@ -2,3 +2,8 @@ module "s3-static-website" {
   source = "../../module/"
   bucket_name  = "staging-static-website-iamvivek"
 }
+
+module "s3-static-website-test" {
+  source = "../../module/"
+  bucket_name  = "staging-static-website-iamvishal"
+}
