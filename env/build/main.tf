d4cb2c25849154d82321d2159f3fdb6164fd7012
vikas <banagevikas@gmail.com>
2023-05-17 17:07:37 +0200

    removed website

--- env/build/main.tf
+++ env/build/main.tf
@@ -1,11 +1,11 @@
-module "s3-static-website" {
-  source = "../../module/"
-  bucket_name  = join("-",["build","staticwebsite-iamaws"])
-}
+# module "s3-static-website" {
+#   source = "../../module/"
+#   bucket_name  = join("-",["build","staticwebsite-iamaws"])
+# }
 
-resource "aws_s3_object" "object" {
-  bucket = module.s3-static-website.s3_bucket_id
-  key    = "index.html"
-  content_type = "text/html"
-  source = "./html/index.html"
-}
+# resource "aws_s3_object" "object" {
+#   bucket = module.s3-static-website.s3_bucket_id
+#   key    = "index.html"
+#   content_type = "text/html"
+#   source = "./html/index.html"
+# }
